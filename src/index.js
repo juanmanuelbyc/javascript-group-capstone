6d877cfd085892f9ce182b728ade828610a6b34a
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-30 18:20:57 -0400

    complete tv Shows cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './index.html';
 import './style.css';
-
 import { getShowsData } from './modules/APImethods.js';
 import { renderShows } from './modules/UImethods.js';
 
 const showsIds = ['tt5491994','tt0903747','tt0795176','tt0185906','tt7366338','tt0306414'];
+
 const showsData = await getShowsData(showsIds);
 
 renderShows(showsData);
--- src/modules/UImethods.js
+++ src/modules/UImethods.js
@@ -5,7 +5,7 @@ export const renderShows = (showsData) => {
     document.querySelector('.showsContainer').innerHTML += `
     <div class="show-card">
     <h3>${show.name}</h3>
-    <h4>${show.premiered}</h4>
+    <h4>${show.premiered.substring(0, 3)}</h4>
     <img src=${show.image.medium}  width="290" height="290"/>
     <p>${show.summary.substring(0, 300)}</p>
     <button type="button">Like!</button>

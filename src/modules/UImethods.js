07ba56bdf099333a09cc1342d903c3bebd80c342
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-30 18:18:45 -0400

    complete tv Shows cards

--- src/modules/UImethods.js
+++ src/modules/UImethods.js
@@ -6,7 +6,10 @@ export const renderShows = (showsData) => {
     <div class="show-card">
     <h3>${show.name}</h3>
     <h4>${show.premiered}</h4>
+    <img src=${show.image.medium}  width="290" height="290"/>
     <p>${show.summary.substring(0, 300)}</p>
+    <button type="button">Like!</button>
+    <button type="button">Details</button>
     </div>`;
   });
 

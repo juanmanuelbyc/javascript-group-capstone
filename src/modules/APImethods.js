d1c8662b44d3c970dde52ee402dc591d3873aaa4
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-31 06:03:32 -0400

    testing involvement api

--- src/modules/APImethods.js
+++ src/modules/APImethods.js
@@ -1,12 +1,51 @@
-const baseUrl = 'https://api.tvmaze.com/lookup/shows?imdb=';
+const tvmazeApiBaseUrl = 'https://api.tvmaze.com/lookup/shows?imdb=';
+const involvementBaseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 
-const getShowsData = async (showsIds) => {
-  const showsData = [];
-  for (let i = 0; i < (showsIds.length); i += 1) {
-    // eslint-disable-next-line max-len, no-await-in-loop
-    await fetch(baseUrl + showsIds[i]).then((data) => data.json()).then((data) => showsData.push(data));
-  }
-  return showsData;
+// export const getShowsData = async (showsIds) => {
+//   let showsData = [];
+//   for (let i=0;i<(showsIds.length);i++) {
+//     await fetch(tvmazeApiBaseUrl+showsIds[i]).then((data) => data.json()).then((data) => showsData.push(data));
+//   }
+//   return showsData;
+// }
+
+
+
+
+
+
+
+
+
+const post = async (data) => {
+  const url = involvementBaseUrl + 'apps/';
+  let response = await fetch(url, {
+    method: 'POST',
+  });
+  console.log(response);
 };
 
-export default getShowsData;
+
+
+const postlike = async (data) => {
+  const xxx = {
+    item_id: 'item1',
+  };
+  const body = JSON.stringify(xxx);
+  const url = involvementBaseUrl + 'apps/iklagzru22zl/likes';
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(xxx),
+  });
+  console.log(response);
+};
+
+
+
+
+
+
+
+// post();
+
+postlike();
